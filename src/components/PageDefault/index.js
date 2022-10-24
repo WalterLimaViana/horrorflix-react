02d6e85be1476f75e991e8229c42b30ee2ba55ee
WalterLimaViana <walter.lima.viana@gmail.com>
2022-10-24 19:54:51 -0300

    criado o componente pagedefault com o cabeçalho e o rodapé

---
+++ src/components/PageDefault/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Footer from '../Footer';
+import Menu from '../menu';
+
+function PageDefault() {
+    return (
+        <div>
+            <Menu></Menu>
+            <Footer></Footer>
+        </div>
+
+    );
+}
+
+export default PageDefault
