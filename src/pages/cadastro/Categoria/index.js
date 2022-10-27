470f883cbd5ac617a41c85f31679288bdf4ba83c
WalterLimaViana <walter.lima.viana@gmail.com>
2022-10-26 21:25:33 -0300

    enviando uma nova categoria para uma lista

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
 
 function CadastroCategoria() {
+    const [categorias, setCategorias] = useState(['Teste']);
+    const [nomeDaCategoria, setNomeDaCategoria] = useState('Valor Inicial');
     return (
         <PageDefault>
-            <h1>Cadastro de Categoria</h1>
+            <h1>Cadastro de Categoria: {nomeDaCategoria}</h1>
 
-            <form>
+            <form onSubmit={function handleSubmit(infosDoEvento) {
+                infosDoEvento.preventDefault();
+                setCategorias([
+                    ...categorias,
+                    nomeDaCategoria
+                ]);
+            }}>
 
                 <label>
                     Nome da Categoria:
                     <input
                         type="text"
+                        value={nomeDaCategoria}
+                        onChange={function funcaoHandlerQueOErroPediu(infosDoEvento) {
+                            setNomeDaCategoria(infosDoEvento.target.value);
+                        }}
                     />
                 </label>
 
@@ -20,6 +32,16 @@ function CadastroCategoria() {
                     Cadastrar Video
                 </button>
             </form>
+
+            <ul>
+                {categorias.map((categoria) => {
+                    return (
+                        <li key={categoria}>
+                            {categoria}
+                        </li>
+                    )
+                })}
+            </ul>
 
 
 
