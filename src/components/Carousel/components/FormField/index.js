be68bd84db8b01bd08d2861e8c2254e9190bc3d6
WalterLimaViana <walter.lima.viana@gmail.com>
2022-10-29 20:47:12 -0300

    criando a function FormField

---
+++ src/components/Carousel/components/FormField/index.js
@@ -0,0 +1,19 @@
+import react from 'react';
+
+function FormField() {
+    return (
+        <div>
+            <label>
+                Nome da Categoria:
+                <input
+                    type="text"
+                    value={values.nome}
+                    name="nome"
+                    onChange={handleChange}
+                />
+            </label>
+        </div>
+    )
+}
+
+export default FormField;
